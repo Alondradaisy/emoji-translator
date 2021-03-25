d5497dc34930dacd42963713183927563975defe
Alondra Warren <daisy.codeimmersives.com>
2021-03-25 12:51:50 -0700

    first commit basic structure of radio buttons

---
+++ main.js
@@ -0,0 +1,7 @@
+const submit = document.querySelector('#submit-button');
+submit.addEventListener('click', submit);
+
+    const input = document.querySelector('#translator-input');
+    input.addEventListener('click', value)
+
+    
